2363c88a097c3ddd66eea0c529372da4d5227388
Chris Fenner <kettletoast@gmail.com>
2021-04-28 20:30:44 -0700

    draft policy helpers implementation

---
+++ pkg/helpers/hashing.go
@@ -0,0 +1,18 @@
+package helpers
+
+import (
+	"crypto"
+	"encoding/binary"
+)
+
+// HashItems returns the hash of the concatenation of the given items with the given algorithm.
+// Uses big-endian byte ordering for integral items.
+func HashItems(alg crypto.Hash, items ...interface{}) ([]byte, error) {
+	h := alg.New()
+	for _, item := range items {
+		if err := binary.Write(h, binary.BigEndian, item); err != nil {
+			return nil, err
+		}
+	}
+	return h.Sum(nil), nil
+}
--- pkg/helpers/policy.go
+++ pkg/helpers/policy.go
@@ -1,10 +1,14 @@
 package helpers
 
 import (
+	"bytes"
 	"crypto"
+	"encoding/binary"
 	"fmt"
 	"github.com/chrisfenner/tpm-spam/pkg/eighttree"
 	"github.com/chrisfenner/tpm-spam/pkg/policypb"
+	"github.com/google/go-tpm/tpm2"
+	"github.com/google/go-tpm/tpmutil"
 )
 
 // NormalizedPolicy represents the normalized OR/AND list-of-lists form of a policy.
@@ -53,11 +57,130 @@ func (t PolicyHashTree) leaf(i int) (*[]byte, error) {
 	return &t[internal+i], nil
 }
 
+func SpamPolicy(alg crypto.Hash) ([]byte, error) {
+	result := make([]byte, alg.Size())
+	return HashItems(alg, result, uint32(0x18f), uint8(0))
+}
+
+func SpamTemplate(index uint32) (*tpm2.NVPublic, error) {
+	if index > 0xffff {
+		return nil, fmt.Errorf("invalid spam index %d (must be a uint16)", index)
+	}
+	policy, err := SpamPolicy(crypto.SHA256)
+	if err != nil {
+		return nil, fmt.Errorf("could not calculate spam policy: %w", err)
+	}
+	// Attribute rationales:
+	//   TPMA_NV_PPWRITE = 0: Can't write with Platform Authorization
+	//   TPMA_NV_OWNERWRITE = 0: Can't write with Owner Authorization
+	//   TPMA_NV_AUTHWRITE = 0: Can't write with Auth Value
+	//   TPMA_NV_POLICYWRITE = 1: Can write with Policy
+	//   TPMA_NV_POLICY_DELETE = 0: Can delete with Platform Authorization
+	//   TPMA_NV_WRITELOCKED = 0: Not write locked (can't be set at creation)
+	//   TPMA_NV_WRITEALL = 1: A partial write of the data is not allowed
+	//   TPMA_NV_WRITEDEFINE = 0: May not be permanently write-locked
+	//   TPMA_NV_WRITE_STCLEAR = 0: May not be write-locked until next boot
+	//   TPMA_NV_GLOBALLOCK = 0: Is not affected by the global NV lock
+	//   TPMA_NV_PPREAD = 0: Can't read with Platform Authorization
+	//   TPMA_NV_OWNERREAD = 0: Can't read with Owner Authorization
+	//   TPMA_NV_AUTHREAD = 1: Can read with Auth Value
+	//   TPMA_NV_POLICYREAD = 0: Can't read with Policy
+	//   TPMA_NV_NO_DA = 1: Exempt from Dictionary Attack logic
+	//   TPMA_NV_ORDERLY = 1: Only required t obe saved when shut down
+	//   TPMA_NV_CLEAR_STCLEAR = 1: TPMA_NV_WRITTEN is cleared by reboot
+	//   TPMA_NV_READLOCKED = 0: Not read locked (can't be set at creation)
+	//   TPMA_NV_WRITTEN = 0: Not already written (can't be set at creation)
+	//   TPMA_NV_PLATFORMCREATE = 1: Undefined with Platform, not Owner Auth
+	//   TPMA_NV_READ_STCLEAR = 0: May not be read-locked
+	attr := tpm2.AttrPolicyWrite |
+		tpm2.AttrWriteAll |
+		tpm2.AttrAuthRead |
+		tpm2.AttrNoDA |
+		tpm2.AttrOrderly |
+		tpm2.AttrClearSTClear |
+		tpm2.AttrPlatformCreate
+	return &tpm2.NVPublic{
+		NVIndex:    tpmutil.Handle(0x017F0000 + index),
+		NameAlg:    tpm2.AlgSHA256,
+		Attributes: attr,
+		AuthPolicy: tpmutil.U16Bytes(policy),
+		DataSize:   64,
+	}, nil
+}
+
+// spamName returns the TPM2B_NAME for a spam NV index.
+func spamName(index uint32, alg crypto.Hash) ([]byte, error) {
+	packed, err := tpmutil.Pack(SpamTemplate(index))
+	if err != nil {
+		return nil, err
+	}
+	hash, err := HashItems(alg, packed)
+	if err != nil {
+		return nil, err
+	}
+	tpmAlg, err := tpm2.HashToAlgorithm(alg)
+	if err != nil {
+		return nil, err
+	}
+	var buf bytes.Buffer
+	if err = binary.Write(&buf, binary.BigEndian, tpmAlg); err != nil {
+		return nil, err
+	}
+	if _, err = buf.Write(hash); err != nil {
+		return nil, err
+	}
+	return buf.Bytes(), nil
+}
+
+// extendSpamPolicy calculates the policy hash for a SpamRule (which is a type of TPM2_PolicyNV)
+func extendSpamPolicy(currentPolicy []byte, rule *policypb.SpamRule, alg crypto.Hash) ([]byte, error) {
+	args, err := HashItems(alg, rule.Operand, rule.Offset, uint16(rule.Comparison))
+	if err != nil {
+		return nil, fmt.Errorf("could not calculate args hash: %w", err)
+	}
+	name, err := spamName(rule.Index, alg)
+	if err != nil {
+		return nil, fmt.Errorf("could not calculate NV index name: %w", err)
+	}
+	return HashItems(alg, currentPolicy, alg, uint32(0x149), args, name)
+}
+
+// ExtendPolicy calculates the policy hash for a Rule, given a starting policy.
+func ExtendPolicy(alg crypto.Hash, currentPolicy []byte, rule *policypb.Rule) ([]byte, error) {
+	switch x := rule.Assertion.(type) {
+	case *policypb.Rule_Spam:
+		return extendSpamPolicy(currentPolicy, x.Spam, alg)
+	default:
+		return nil, fmt.Errorf("unrecognized rule type: %v", x)
+	}
+}
+
 // leafPolicy calculates the TPM policy hash for the given sequence of leaf rules, with the
 // specified algorithm, storing it into policy.
 func leafPolicy(policy *[]byte, rules []*policypb.Rule, alg crypto.Hash) error {
-	// TODO: calculate the policy hash of all the rules according to the TPM specification.
+	result := make([]byte, alg.Size())
+	for i, rule := range rules {
+		var err error
+		result, err = ExtendPolicy(alg, result, rule)
+		if err != nil {
+			return fmt.Errorf("could not calculate rule %d policy hash: %w", i, err)
+		}
+	}
+	*policy = result
 	return nil
+}
+
+// policyChildren returns the digest of all the children of the given internal node. If the node is a leaf node, returns nil.
+func policyChildren(t PolicyHashTree, index int) [][]byte {
+	start := eighttree.ChildIndex(index, 0)
+	if start >= len(t) {
+		return nil
+	}
+	end := eighttree.ChildIndex(index, 7)
+	if end > len(t) {
+		end = len(t)
+	}
+	return t[start:end]
 }
 
 // internalPolicy calculates the TPM policy hash for the given internal node, with the specified
@@ -65,7 +188,13 @@ func leafPolicy(policy *[]byte, rules []*policypb.Rule, alg crypto.Hash) error {
 // This function works on partially calculated policy trees, but it requires that all nodes after
 // the given node have already been calculated.
 func internalPolicy(policy *[]byte, t PolicyHashTree, index int, alg crypto.Hash) error {
-	// TODO: calculate the PolicyOR of all the children according to the TPM specification.
+	children := policyChildren(t, index)
+	result := make([]byte, alg.Size())
+	result, err := HashItems(alg, result, uint32(0x171), children)
+	if err != nil {
+		return err
+	}
+	*policy = result
 	return nil
 }
 
